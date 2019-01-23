bd190ee4772f9cf42b352b2a130f7f402cdc8220
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-01-23 22:01:50 +0800

    style tweaks and correction

--- src/config/typography.js
+++ src/config/typography.js
@@ -1,14 +1,42 @@
 import Typograph from 'typography';
 
 const typography = new Typograph({
-  headerFontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans serif'],
-  bodyFontFamily: ['Georgia', 'serif'],
+  headerFontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica',
+    'Arial',
+    'sans serif'
+  ],
+  bodyFontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Georgia',
+    'serif'
+  ],
   googleFonts: [
     {
       name: 'Material Icons',
       styles: ['400']
     }
-  ]
+  ],
+  overrideStyles: ({ rhythm }) => ({
+    blockquote: {
+      color: '#000000',
+      marginLeft: 0,
+      marginRight: 0,
+      paddingLeft: rhythm(1 / 2),
+      paddingRight: rhythm(2 / 3),
+      paddingTop: rhythm(1 / 2),
+      paddingBottom: rhythm(1 / 2),
+      borderLeftColor: '#8d8d8d',
+      borderLeftWidth: rhythm(1 / 2),
+      borderLeftStyle: 'solid'
+    }
+  })
 });
 
 export default typography;
